f3da8c781c69ac23c4abf543f0aa7fec1f7dc13c
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2014-06-25 11:52:04 -0700

    AUI-1412 Removed aui-panel dependecy, added yui-panel

--- src/aui-color-picker-deprecated/js/aui-color-picker-base-deprecated.js
+++ src/aui-color-picker-deprecated/js/aui-color-picker-base-deprecated.js
@@ -561,7 +561,7 @@ var ColorPicker = A.Component.create({
        ]
                 }).render(instance.get('contentBox'));
 
-                var bodyNode = container.bodyNode;
+                var bodyNode = container._stdModNode;
 
                 bodyNode.addClass(CSS_CONTAINER);
 
