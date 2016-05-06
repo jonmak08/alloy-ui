5449dfa668b092a0a34f28f5819f8c14c07093b0
Cleydyr de Albuquerque <cleydyr.albuquerque@liferay.com>
2016-05-06 15:08:46 -0300

    AUI-1783 Add enter key input event listener to the Datepicker module

--- src/aui-datepicker/js/aui-datepicker.js
+++ src/aui-datepicker/js/aui-datepicker.js
@@ -200,6 +200,9 @@ A.mix(DatePickerBase.prototype, {
         instance.set(ACTIVE_INPUT, node);
 
         instance.alignTo(node);
+
+        node.on('keydown', instance._handleKeydownEvent, instance);
+
         instance.clearSelection(true);
         instance.selectDates(instance.getParsedDatesFromInputValue());
     },
@@ -246,6 +249,33 @@ A.mix(DatePickerBase.prototype, {
 
         instance._setCalendarToFirstSelectedDate();
     },
+
+    /* TODO. Wanna help? Please send a Pull Request.
+     *
+     * @method _fireEnterKey
+     * @protected
+     */
+    _fireEnterKey: function() {
+        var instance = this;
+
+        instance.fire('enterKey');
+    },
+
+    /**
+    * Handles keydown events
+    *
+    * @method _handleKeydownEvent
+    * @protected
+    */
+    _handleKeydownEvent: function(event) {
+        var instance = this;
+
+        if (event.isKey('ENTER')) {
+            instance._fireEnterKey();
+        }
+    },
+
+    /**
 
     /**
      * TODO. Wanna help? Please send a Pull Request.
