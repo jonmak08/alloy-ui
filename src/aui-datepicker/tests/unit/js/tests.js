88354f2fc49ecc64094231622e666fe0f23a4e9b
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2016-06-23 15:01:31 -0300

    AUI-1783 Add test

--- src/aui-datepicker/tests/unit/js/tests.js
+++ src/aui-datepicker/tests/unit/js/tests.js
@@ -6,6 +6,38 @@ YUI.add('module-tests', function(Y) {
         name: 'Automated Tests',
         'test is empty': function() {
             Y.Assert.pass('No Tests Provided For This Module');
+        },
+
+        'enterKey event should fire when enter key is pressed in the active input': function() {
+            var enterKeydownCount = 0,
+                triggers = [
+                    '#inputTriggerA',
+                    '#inputTriggerB',
+                    '#inputTriggerC',
+                    '#inputTriggerB',
+                    '#inputTriggerA'
+                ];
+
+            this.datePicker = new Y.DatePicker({
+                on: {
+                    enterKey: function() {
+                        enterKeydownCount++;
+                    }
+                },
+                trigger: triggers.join()
+            });
+
+            triggers.forEach(function(trigger) {
+                trigger = Y.one(trigger);
+
+                trigger.focus();
+
+                trigger.simulate('keydown', {
+                    keyCode: 13
+                });
+            });
+
+            Y.Assert.areEqual(5, enterKeydownCount);
         }
     }));
 
